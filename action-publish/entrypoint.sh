09e6a797d73f53b4f3aa4b584a36263ccb09598f
Denis Croitoru <dennis.croitoru@gmail.com>
2020-05-20 13:49:50 +0300

    Update entrypoint.sh

--- action-publish/entrypoint.sh
+++ action-publish/entrypoint.sh
@@ -3,6 +3,7 @@
 COMMITTER_USER_NAME="$(git log --format='%an' -1)"
 COMMITTER_EMAIL="$(git log --format='%ae' -1)"
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo repo token is $INPUT_repo-token"
 sh -c "echo COMMITTER_USER_NAME is ${COMMITTER_USER_NAME}"
 sh -c "echo COMMITTER_EMAIL is ${COMMITTER_EMAIL}"
 sh -c "echo GITHUB_TOKEN is ${GITHUB_TOKEN}"
